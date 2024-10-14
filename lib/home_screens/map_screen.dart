5356419ce14253b06de67be90836f0266f4686bc
HudaOmer <hudaoe175@gmail.com>
2024-10-15 01:52:53 +0200

    Add Map Screen

--- lib/home_screens/map_screen.dart
+++ lib/home_screens/map_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  late GoogleMapController mapController;
+
+  static const LatLng _center = LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Google Maps in Flutter')),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        mapType: MapType.terrain,
+        initialCameraPosition: const CameraPosition(
+          target: _center,
+          zoom: 11.0,
+        ),
+      ),
+    );
   }
 }
