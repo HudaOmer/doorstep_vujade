7642d6e95deb68133106d4409f79dbb7f89636e0
HudaOmer <hudaoe175@gmail.com>
2024-11-13 14:28:49 +0200

    Add Apartment Screen

--- lib/apartment/ratings_screen.dart
+++ lib/apartment/ratings_screen.dart
@@ -23,32 +23,29 @@ class RatingsScreen extends StatelessWidget {
             child: const Icon(Icons.arrow_back_ios),
             onTap: () => Navigator.pop(context)),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Column(children: [
-            Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(
-                    5,
-                    (index) => const Icon(Icons.star_outline_rounded,
-                        size: 40, color: Colors.grey))),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.8,
-              padding: const EdgeInsets.only(right: 10, left: 10),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  border: Border.all(color: Colors.grey)),
-              child:
-                  const TextField(showCursor: false, minLines: 4, maxLines: 6),
-            ),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.05),
-          ]),
-          Center(
-              child: ColoredButton(
-                  color: mainColor, text: 'Post', onPressed: () {})),
-        ],
-      ),
+      body: Column(children: [
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(
+                5,
+                (index) => const Icon(Icons.star_outline_rounded,
+                    size: 40, color: Colors.grey))),
+        const SizedBox(height: 30),
+        const Row(children: [SizedBox(width: 40), Text("Comment")]),
+        const SizedBox(height: 10),
+        Container(
+          width: MediaQuery.of(context).size.width * 0.8,
+          padding: const EdgeInsets.only(right: 10, left: 10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              border: Border.all(color: Colors.grey)),
+          child: const TextField(showCursor: false, minLines: 4, maxLines: 6),
+        ),
+        SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+        Center(
+            child: ColoredButton(
+                color: mainColor, text: 'Post', onPressed: () {})),
+      ]),
     );
   }
 }
