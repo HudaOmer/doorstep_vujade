ae84f5a29013d23c3c0bde06404d08be3af6720e
HudaOmer <hudaoe175@gmail.com>
2024-11-13 14:29:56 +0200

    Add to chat Screen

--- lib/chat_screens/all_chats_screen.dart
+++ lib/chat_screens/all_chats_screen.dart
@@ -1,10 +1,8 @@
+import 'package:doorstep_vujade/global_widgets/custom_icon.dart';
 import 'package:flutter/material.dart';
-import '../apartment/apartment_requests_screen.dart';
-import '../apartment/favorite_list_screen.dart';
-import '../apartment/saved_categories_screen.dart';
-import '../global_widgets/colored_button.dart';
+import '../global_widgets/custom_search_bar.dart';
 import '../utils/colors.dart';
-import '../auth_screens/edit_profile_screen.dart';
+import 'chat_screen.dart';
 
 double appbarheight = 80;
 
@@ -21,44 +19,60 @@ class AllChatsScreen extends StatelessWidget {
         iconTheme: IconThemeData(color: contrastColor),
         backgroundColor: Colors.white,
         title: const Center(
-            child: Text('Filter      ', style: TextStyle(fontSize: 20))),
+            child: Text('Chat      ', style: TextStyle(fontSize: 20))),
         leading: const Icon(Icons.arrow_back_ios),
       ),
       body: ListView(children: [
-        const CustomListTile(
-            title: 'Profile',
-            icon: Icons.manage_accounts,
-            screen: EditProfileScreen()),
-        const CustomListTile(
-            title: 'Favorites',
-            icon: Icons.favorite_rounded,
-            screen: FavoriteListScreen()),
-        const CustomListTile(
-            title: 'Saved',
-            icon: Icons.bookmark,
-            screen: SavedCategoriesScreen()),
-        const CustomListTile(
-            title: 'Settings',
-            icon: Icons.settings,
-            screen: ApartmentRequestsScreen()),
-        SizedBox(height: MediaQuery.of(context).size.height * 0.35),
-        Center(
-            child: ColoredButton(
-                color: mainColor, text: 'Log Out', onPressed: () {})),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+                width: MediaQuery.of(context).size.width * 0.88,
+                child: const CustomSearchBar()),
+          ],
+        ),
+        SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+        const ChatListTile(
+            sender: 'SomeOne',
+            screen: ChatScreen(),
+            profile: '',
+            message: 'hello there how are you?'),
+        const ChatListTile(
+            sender: 'SomeOne3',
+            screen: ChatScreen(),
+            profile: 'assets/images/villa.jpeg',
+            message: 'no'),
+        const ChatListTile(
+            sender: 'SomeOne4',
+            screen: ChatScreen(),
+            profile: '',
+            message: 'bla bla'),
+        const ChatListTile(
+            sender: 'SomeOne2',
+            screen: ChatScreen(),
+            profile: '',
+            message: 'ok'),
+        SizedBox(height: MediaQuery.of(context).size.height * 0.05),
       ]),
     );
   }
 }
 
-class CustomListTile extends StatelessWidget {
-  final String title;
-  final IconData icon;
+class ChatListTile extends StatelessWidget {
+  final String sender;
+  final String profile;
+  final String message;
+  final bool? isRead;
+  final int? unreadNo;
   final Widget screen;
-  const CustomListTile(
+  const ChatListTile(
       {super.key,
-      required this.title,
-      required this.icon,
-      required this.screen});
+      required this.sender,
+      required this.profile,
+      required this.screen,
+      required this.message,
+      this.isRead,
+      this.unreadNo});
 
   @override
   Widget build(BuildContext context) {
@@ -66,25 +80,27 @@ class CustomListTile extends StatelessWidget {
         padding: const EdgeInsets.all(10.0),
         child: ListTile(
           title:
-              Text(title, style: const TextStyle(fontWeight: FontWeight.w500)),
-          leading: Container(
-              width: 42,
-              height: 42,
-              margin: const EdgeInsets.only(left: 20),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.black.withOpacity(0.1),
-                      spreadRadius: 1,
-                      blurRadius: 4,
-                      offset: const Offset(0, -2)),
-                ],
-              ),
-              child: Icon(icon, size: 30, color: contrastColor)),
-          trailing:
-              const Icon(Icons.arrow_forward_ios, size: 20, color: Colors.grey),
+              Text(sender, style: const TextStyle(fontWeight: FontWeight.w500)),
+          subtitle: Text(message,
+              style: const TextStyle(fontWeight: FontWeight.w400)),
+          leading: CircleAvatar(
+              radius: 30,
+              child: profile.isNotEmpty
+                  ? SizedBox(
+                      width: 60,
+                      height: 60,
+                      child:
+                          CustomIcon(iconName: profile, height: 60, width: 60))
+                  : const SizedBox(width: 30)),
+          trailing: const Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text("2:00 PM",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500, color: Colors.grey)),
+              Icon(Icons.done_all, size: 20, color: Colors.grey),
+            ],
+          ),
           onTap: () => Navigator.push(
               (context), MaterialPageRoute(builder: (context) => screen)),
         ));
